09de98173e14f4ec4150e8d74765e5cd7d4f752d
skateboardant <ley27510@gmail.com>
2026-04-30 20:37:58 +0800

    Argmax function implementation

--- src/argmax.s
+++ src/argmax.s
@@ -17,18 +17,40 @@
 argmax:
 
     # Prologue
+    addi sp, sp, -4
+    sw ra, 0(sp)
 
+    addi t0, x0, 1
+    blt a1, t0, exit
+
+    li t1, 0
+    lw t2, 0(a0)
+    li t3, 1
 
 loop_start:
-
+    beq t3, a1, loop_end
+    slli t4, t3, 2
+    add t5, a0, t4
+    lw t6, 0(t5)
+    bgt t6, t2, change
 
 loop_continue:
-
-
+    addi t3, t3, 1
+    j loop_start
 loop_end:
-    
-
+    add a0, t1, x0
+ 
     # Epilogue
-
+    lw ra, 0(sp)
+    addi sp, sp, 4
 
     ret
+
+change:
+    add t2, t6, x0
+    add t1, t3, x0
+    j loop_continue
+exit:
+    li a0, 17
+    li a1, 77
+    ecall
